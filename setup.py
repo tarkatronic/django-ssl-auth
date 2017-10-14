e318e2a76b0e4388d67c12106fee3ded12e3b37f
Joey Wilhelm <tarkatronic@gmail.com>
2017-10-14 09:32:08 -0700

    Revamp/update the setup

--- setup.py
+++ setup.py
@@ -1,13 +1,54 @@
-#!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='django-ssl-auth',
-      version='1.0.1',
-      description='Django SSL Client Authentication',
-      author='Kimmo Parviainen-Jalanko',
-      author_email='kimvais@ssh.com',
-      url='https://github.com/kimvais/django-ssl-client-auth/',
-      packages=['django_ssl_auth'],
-      requires=['Django (>=1.8)']
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('README.rst', 'r') as f:
+    README = f.read()
+
+with open('VERSION', 'r') as vfile:
+    VERSION = vfile.read().strip()
+
+
+setup(
+    name='django-ssl-auth',
+    version=VERSION,
+    description='Django SSL Client Authentication',
+    long_description=README,
+    author='Kimmo Parviainen-Jalanko',
+    author_email='kimvais@ssh.com',
+    maintainer='Joey Wilhelm',
+    maintainer_email='tarkatronic@gmail.com',
+    license='MIT',
+    url='https://github.com/tarkatronic/django-ssl-auth/',
+    download_url='https://github.com/tarkatronic/django-ssl-auth/archive/master.tar.gz',
+    packages=['django_ssl_auth'],
+    include_package_data=True,
+    install_requires=['Django>=1.8'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe=False,
+    test_suite='runtests.runtests'
 )
